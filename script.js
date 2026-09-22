b395003ca9a2207814e457c60317bdd4aed106dd
reco052002-pixel <reco052002@gmail.com>
2026-09-23 03:21:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Rename this file to: script.js
-
 document.querySelectorAll('a[href^="#"]').forEach(link => {
+
   link.addEventListener("click", event => {
+
     const href = link.getAttribute("href");
+
     if (href === "#") return;
 
     const target = document.querySelector(href);
+
     if (target) {
+
       event.preventDefault();
-      target.scrollIntoView({ behavior: "smooth" });
+
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+
     }
+
   });
+
 });
